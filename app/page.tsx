46b9d90500b735ec3d5632966167cdf824d519d2
srivishnu25 <thatisrivishnu@gmail.com>
2025-01-02 23:06:39 +0530

    Update ESLint configuration, modify Next.js config to ignore ESLint during builds, and clean up imports in various components for improved readability.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import styles from "@/app/ui/home.module.css";
 import { lusitana } from "@/app/ui/fonts";
 import AcmeLogo from "@/app/ui/acme-logo";
 import Image from "next/image";
